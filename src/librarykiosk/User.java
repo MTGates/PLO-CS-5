22fe38de04339a5fed947c5ae2f550d3eba835e2
MTGates <mtagates@gmail.com>
2026-07-15 23:31:01 -0600

    Completed Book and User classes

--- src/librarykiosk/User.java
+++ src/librarykiosk/User.java
@@ -1,5 +1,28 @@
 package librarykiosk;
+import java.util.HashMap;
 
-public class User {
-
+public class User extends Person {
+	
+	public User(int id, String name) {
+		super(id, name);
+	}
+	
+	public boolean validateId() {
+		
+		// Creates user ID/name pairs using HashMap
+		HashMap<Integer, String> validUsers = new HashMap<>();
+		
+		validUsers.put(101, "Morgan");
+		validUsers.put(202, "Ty");
+		validUsers.put(303, "Montana");
+			
+		int id = getId();
+		
+		if (validUsers.containsKey(id)) {
+				return true;
+			}
+		
+		// id is not a valid user ID
+		return false;
+	}
 }
