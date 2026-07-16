f4ad08b4e22795e989018fe0f8dbb094f2b5d5f4
SummerSiren2331 <bfidler2007@gmail.com>
2026-07-16 16:39:46 -0600

    minor tweaks to file structure in preparation for implementation

--- src/libraryJFrame/LibraryFrame.java
+++ src/libraryJFrame/LibraryFrame.java
@@ -6,6 +6,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.JTextPane;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class LibraryFrame extends JFrame {
 
@@ -37,6 +43,12 @@ public class LibraryFrame extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblPleaseID = new JLabel("Please enter your Library ID number:");
+		lblPleaseID.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPleaseID.setFont(new Font("Segoe UI", Font.BOLD, 24));
+		contentPane.add(lblPleaseID, BorderLayout.NORTH);
 
 	}
 
