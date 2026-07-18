1c441b149727eb749856ac5cfce68bdddd3ddab6
SummerSiren2331 <bfidler2007@gmail.com>
2026-07-17 23:22:56 -0600

    Created file structure for all planned frames, 3 & 4 are missing all implementation.

---
+++ src/libraryJFrame/LibraryFrameFour.java
@@ -0,0 +1,47 @@
+package libraryJFrame;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+/**
+* @author - Morgan Fidler
+*/
+//TODO: missing logical implementation
+
+public class LibraryFrameFour extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LibraryFrameFour frame = new LibraryFrameFour();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LibraryFrameFour() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+
+	}
+
+}
---
+++ src/libraryJFrame/LibraryFrameThree.java
@@ -0,0 +1,47 @@
+package libraryJFrame;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+/**
+* @author - Morgan Fidler
+*/
+//TODO: missing GUI & logical implementation
+
+public class LibraryFrameThree extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LibraryFrameThree frame = new LibraryFrameThree();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LibraryFrameThree() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+
+	}
+
+}
